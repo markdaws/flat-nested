afae66d64fd0646465f78bb6ce92d16a53bfa55f
Mark Dawson <markdawson@live.com>
2012-03-31 00:28:32 -0700

    update tests

--- tests.js
+++ tests.js
@@ -16,47 +16,6 @@ dataProvider.write = function(data, callback) {
     }, 1000);
 };
 
-
-var fetchProcessAndWriteData = function(data, callback) {
-    
-    dataProvider.read(data.url, function(error, result) {
-        if(error) {
-            callback(error);
-            return;
-        }
-        
-        var total = 0;
-        var largest = 0;
-        for(var i=0; i<result.length; ++i) { 
-            total += result[i]; 
-            largest = Math.max(largest, result[i]);
-        };
-
-        var info = 'total:' + data.total + ', largest:' + data.largest;
-        dataProvider.write(info, function(error, bytesWritten) {
-            if(error) {
-                callback(error);
-                return;
-            }
-            
-            callback(null, bytesWritten);
-        });
-    });
-};
-
-var writeData = function(data, callback) {
-    
-    var info = 'total:' + data.total + ', largest:' + data.largest;
-    dataProvider.write(info, function(error, bytesWritten) {
-        if(error) {
-            callback(error);
-            return;
-        }
-
-        callback(null, bytesWritten);
-    });
-};
-
 var fetchAndProcessData = function(data, callback) {
     dataProvider.read(data.url, function(error, result) {
         if(error) {
@@ -119,7 +78,7 @@ var writeData2 = function(data, callback) {
     
     var info = 'total:' + data.total + ', largest:' + data.largest;
     dataProvider.write(info, _$.ce(function(bytesWritten) {
-        callback(2, null, bytesWritten);
+        callback(null, bytesWritten);
     }));
 };
 
